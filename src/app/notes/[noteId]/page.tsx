3d02f6a1df1f5fa59b098f030bbdfa7719a5084d
Jallok <anggawiranata3106@gmail.com>
2024-08-11 05:36:56 +0700

    metadata, dinamyc metadata

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,15 +1,29 @@
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "Note Detail Page",
+//   description: "This is Note Detail Page",
+// };
+
+type Props = {
+  params: {
+    noteId: string
+  }
+}
+
+// dinamic metadata
+export const generateMetadata = ({params}: Props): Metadata => {
+  return {
+    title: `Note Detail ${params.noteId}`
+  }
+}
 
 export default function NoteDetail({
   params,
-}: {
-  params: {
-    noteId: string;
-  };
-})
-{
-  if(parseInt(params.noteId) > 100){
-    notFound()
+}: Props) {
+  if (parseInt(params.noteId) > 100) {
+    notFound();
   }
   return <h1>Note Detail {params.noteId}</h1>;
 }
