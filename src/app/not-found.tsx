16692f30c6a84ce4ab9738fcdc480b76e57e86cd
Jallok <anggawiranata3106@gmail.com>
2024-08-10 10:45:06 +0700

    nested not found error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,3 @@
 export default function NotFound() {
-  return <h1>Sorry your page is not available!</h1>;
+  return <h1 className="text-center">Sorry your page is not available!</h1>;
 }
--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 export default function NoteDetail({
   params,
 }: {
   params: {
     noteId: string;
   };
-}) {
+})
+{
+  if(parseInt(params.noteId) > 100){
+    notFound()
+  }
   return <h1>Note Detail {params.noteId}</h1>;
 }
